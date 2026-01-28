18ce6c7d8dec6509ad22b0c957eaa2b605ec081c
bbelli <jb_belli@esi.dz>
2026-01-28 08:27:16 +0100

    updated database to add admin authentication

--- db.js
+++ db.js
@@ -2,17 +2,25 @@ const Database = require("better-sqlite3");
 
 const db = new Database("leads.db");
 
-// Create table if not exists
 db.prepare(`
-  CREATE TABLE IF NOT EXISTS leads (
+    CREATE TABLE IF NOT EXISTS leads (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT,
     email TEXT,
     phone TEXT,
     address TEXT,
     message TEXT,
+    status TEXT DEFAULT 'new',
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-  )
+    )
+`).run();
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS admins (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT UNIQUE,
+    password TEXT
+    )
 `).run();
 
 module.exports = db;
